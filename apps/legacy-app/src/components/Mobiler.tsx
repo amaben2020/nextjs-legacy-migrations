a4605c05a23a8dfe71b69857e7814d78dc4691a0
amaben2020 <60056927+amaben2020@users.noreply.github.com>
2025-09-14 15:27:15 +0100

    update

--- apps/legacy-app/src/components/Mobiler.tsx
+++ apps/legacy-app/src/components/Mobiler.tsx
@@ -1,9 +1,16 @@
+// import React from 'react';
+// import { useIsMobile } from './../../../../packages/mobile/src/useIsMobile';
+
+// const Mobiler = () => {
+//   const mobile = useIsMobile();
+//   return <div>Mobiler {JSON.stringify(mobile)}</div>;
+// };
+
+// export default Mobiler;
 import React from 'react';
-import { useIsMobile } from './../../../../packages/mobile/src/useIsMobile';
 
 const Mobiler = () => {
-  const mobile = useIsMobile();
-  return <div>Mobiler {JSON.stringify(mobile)}</div>;
+  return <div>Mobiler</div>;
 };
 
 export default Mobiler;
